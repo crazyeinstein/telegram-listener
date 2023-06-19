9f377438eef7777d87b19e86936603be4593de25
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2023-06-19 11:50:46 +0300

    Added config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import * as config from 'config';
+
+const WEB_SERVER_PORT: number = config.get<number>('webServer.port');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(WEB_SERVER_PORT, () => {
+    console.log(`Application listening on port ${WEB_SERVER_PORT}`);
+  });
 }
+
 bootstrap();
